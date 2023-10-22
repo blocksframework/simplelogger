a298599f21f8686c8c3c979d0d4c9a556d6e8817
Stephen Stephenson <stephenson.inbox@gmail.com>
2023-10-22 19:55:39 +0200

    Feat: Add command line title output

--- src/class/Helper/CommandLineOutput.php
+++ src/class/Helper/CommandLineOutput.php
@@ -13,4 +13,21 @@ class CommandLineOutput {
     public static function success( $text ) {
         echo "\033[00;32m{$text}\033[0m".PHP_EOL;
     }
+
+    public static function title( $text ) {
+        $text_length = mb_strlen( $text );
+
+        $terminal_width = self::getTerminalWidth();
+
+        $equal_signs = str_repeat( '=', $terminal_width - $text_length - 5 );
+
+        echo "=== {$text} {$equal_signs}".PHP_EOL;
+    }
+
+    private static function getTerminalWidth() {
+        $terminal_width = intval( shell_exec( 'tput cols' ) );
+
+        // If tput cols doesn't work, use a default width (80 characters)
+        return ( $terminal_width > 0 ) ? $terminal_width : 80;
+    }
 }
