1ae84f12209c245eb113cb1fb0d1e0eff89567df
Stephen Stephenson <stephenson.inbox@gmail.com>
2023-12-26 02:25:28 +0100

    Refactor: Template

--- src/class/View/Template.php
+++ src/class/View/Template.php
@@ -5,33 +5,28 @@ namespace Blocks\System\View;
 use Blocks\System\View;
 
 class Template extends View {
-    protected $template_path;
+    protected $path;
 
-    public function __construct( string $template_path ) {
-        if ( empty( $template_path ) ) {
-            throw new \Exception( 'Class member $template_path can\'t be undefined or an empty string' );
+    public function __construct( string $path ) {
+        if ( empty( $path ) ) {
+            throw new \Exception( 'Class member $path can\'t be undefined or an empty string' );
         }
 
-        $this->template_path = $template_path;
+        $this->path = $path;
     }
 
     public function render(): ?string {
-        $corrected_template_relative_path = ltrim( $this->template_path, '/' );
-
-        $file = DIR_MODULE.$corrected_template_relative_path;
-
-        if ( file_exists( $file ) ) {
+        if ( file_exists( $path ) ) {
             extract( $this->data );
 
             ob_start();
-
-            require $file;
+            require $path;
             $content = ob_get_contents();
             ob_end_clean();
 
             return $content;
         }
 
-        throw new \Exception( 'Template->fetch(): could not load template "'.$file.'"' );
+        throw new \Exception( 'Template->fetch(): could not load template "'.$path.'"' );
     }
 }
